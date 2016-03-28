4e67fc65af68fc52c0d796a02dc41a994b7a21e9
Mehmet Gök <meetgok@gmail.com>
2016-03-28 04:51:33 +0300

    Update

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+var middleware = {
+	requireAuthentication : function(req, res, next) {
+		console.log('private');
+		next();
+	},
+	logger : function(req, res, next) {
+		var date = new Date().toString();
+		console.log(req.method + ' ' + req.originalUrl + ' ' + date);
+		next();
+	}
+};
+
+app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function() {
+	console.log('server started on port ' + PORT);
+});
